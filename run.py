5d8cb669a872350781ec7a4494af038511f7895a
Matthew <matthew.poyner79@gmail.com>
2019-12-06 13:57:38 +1300

    contact form put in

--- run.py
+++ run.py
@@ -1,25 +1,44 @@
-import os 
+import os
 import json
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 @app.route('/about')
 def about():
-    data=[]
+    data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
-    return render_template("about.html", page_title="About", list_of_numbers=[1,2,3], company=data)
+    return render_template("about.html", page_title="About", company=data)
 
-@app.route('/contact')
+
+@app.route('/about/<member_name>')
+def about_member(member_name):
+    member = {}
+   
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+   
+    return render_template("member.html", member=member)
+
+
+@app.route('/contact', methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form["name"])
     return render_template("contact.html", page_title="Contact")
 
-@app.route("/careers")
+
+@app.route('/careers')
 def careers():
     return render_template("careers.html", page_title="Careers")
 
